9aabee30143107d2d4b079175fa4344a881d9a59
DylanVosper <7841@balcarras.gloucs.sch.uk>
2024-09-16 14:30:19 +0000

    basic login and signup page

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-from flask import Flask
+from flask import Flask, render_template
+
+
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'THISISABADKEY'
 
+##routing
+@app.route("/")
+def home():
+    return  render_template('index.html')
+
+@app.route("/signup")
+def signUp():
+    return  render_template('signup.html')
 
 
 
